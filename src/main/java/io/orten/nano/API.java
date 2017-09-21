541510bce00c83e872fd66ee2f2c714ae1b0bc37
Marcus Münger <marcus.munger@cybercom.com>
2017-09-21 13:42:53 +0200

    Initial commit

---
+++ src/main/java/io/orten/nano/API.java
@@ -0,0 +1,19 @@
+package io.orten.nano;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class API
+{
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response testGet()
+    {
+        return Response.ok("Test").build();
+    }
+}
