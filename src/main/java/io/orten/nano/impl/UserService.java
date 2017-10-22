4b1cccae8a3eeeb074b74c8d5379e93deea7cb80
YahyaDari <32179552+YahyaDari@users.noreply.github.com>
2017-10-22 14:18:11 +0200

    Add files via upload
    Refactor according to Marcus comments

--- src/main/java/io/orten/nano/impl/UserService.java
+++ src/main/java/io/orten/nano/impl/UserService.java
@@ -6,35 +6,58 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
 
-        public static List<User> u_list = new ArrayList<User>();
+        public static List<User> userList = new ArrayList<User>();
 
         public static User getUser(long userID) throws Exception {
-            Session session = Database.getSession();
-            User user = session.get(User.class, userID);
-            session.close();
-            return user;
+            Session session = null;
+            try {
+                session = Database.getSession();
+                User user= session.get(User.class, userID);
+                session.close();
+                return user;
+            }
+            catch (HibernateException e) {
+                throw e;
+            } finally {
+                if (session != null) session.close();
+            }
         }
 
         public static List<User> getUsers() throws Exception {
-            Session session = Database.getSession();
-            List users = session.createQuery("from User").list();
-            session.close();
-            return users;
+            Session session = null;
+            try {
+                session = Database.getSession();
+                List users = session.createQuery("from User").list();
+                session.close();
+                return users;
+            }
+            catch (HibernateException e) {
+                throw e;
+            }
+            finally {
+                if (session != null) session.close();
+            }
         }
 
         public static List getUsersByName(String userName) throws Exception {
-            Session session = Database.getSession();
-            Query query = session.createQuery("from User where userName like :userName");
-            query.setParameter("userName", "%" + userName + "%");
-            List<User> users = query.list();
-            session.close();
-            return users;
+            Session session = null;
+            try {
+                session = Database.getSession();
+                Query query = session.createQuery("from User where userName like :userName");
+                query.setParameter("userName", "%" + userName + "%");
+                List<User> users = query.list();
+                session.close();
+                return users;
+            } catch (HibernateException e) {
+                throw e;
+            } finally {
+                if (session != null) session.close();
+            }
         }
 
         public static void saveUser(User user) throws Exception {
@@ -42,20 +65,21 @@ public class UserService {
             Transaction tx = null;
             try {
                 tx = session.beginTransaction();
-                session.save(user);
+                session.saveOrUpdate(user);
                 tx.commit();
             } catch (HibernateException e) {
                 if (tx != null) tx.rollback();
                 throw e;
             } finally {
-                session.close();
+                if (session != null) session.close();
             }
         }
 
-        public static void deleteUser(String userID) throws Exception {
-            Session session = Database.getSession();
+        public static void deleteUser(long userID) throws Exception {
+            Session session = null;
             Transaction tx = null;
             try {
+                session = Database.getSession();
                 tx = session.beginTransaction();
                 User user= session.get(User.class, userID);
                 session.delete(user);
@@ -64,7 +88,7 @@ public class UserService {
                 if (tx != null) tx.rollback();
                 throw e;
             } finally {
-                session.close();
+                if (session != null) session.close();
             }
         }
 }
