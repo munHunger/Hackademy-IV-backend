847cae0aa205ff6bbc154b3ebea690eef6e9b4b0
Jabbar-Hussain <32177655+Jabbar-Hussain@users.noreply.github.com>
2017-10-22 11:17:27 +0200

    Refactoring Project Classes

---
+++ ProjectAPI.java
@@ -0,0 +1,86 @@
+package io.orten.nano.business;
+
+import io.orten.nano.impl.ProjectService;
+import io.orten.nano.model.Project;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/")
+public class ProjectAPI{
+    public static List<Project> projectList = new ArrayList<Project>();
+
+    @GET
+    @Path("/getprojectbyid/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getprojectbyid(@PathParam("id") Long id) {
+        try {
+            Project project = ProjectService.getprojectbyid(id);
+            return Response.status(HttpServletResponse.SC_OK).entity(project).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/getprojectbyprojectid/{projectID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProjectByProjectId(@PathParam("projectID") String projectID) {
+        try {
+            Project project = ProjectService.getProjectByProjectId(projectID);
+            return Response.status(HttpServletResponse.SC_OK).entity(project).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/getlistofprojects")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getListOfProjects() {
+        try {
+            List<Project> projects = ProjectService.getListOfProjects();
+            return Response.status(HttpServletResponse.SC_OK).entity(projects).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/getlistofprojectsbyname/{projectName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProjectsByName(@PathParam("projectName") String projectName) {
+        try {
+            List<Project> projects = ProjectService.getProjectsByName(projectName);
+            return Response.status(HttpServletResponse.SC_OK).entity(projects).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @POST
+    @Path("/saveproject")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveProject(Project project) {
+        try {
+            ProjectService.saveProject(project);
+            return Response.status(HttpServletResponse.SC_OK).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DELETE
+    @Path("/deleteproject/{id}")
+    public Response deleteProject(@PathParam("id") Long id) {
+        try {
+            ProjectService.deleteProject(id);
+            return Response.status(HttpServletResponse.SC_OK).build();
+        } catch (Exception e) {
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
