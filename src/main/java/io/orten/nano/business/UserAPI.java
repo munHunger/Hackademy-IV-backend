455fde7ff9c79e935a133fd09b965d502eec1aaf
YahyaDari <32179552+YahyaDari@users.noreply.github.com>
2017-10-22 14:17:36 +0200

    Add files via upload
    Refactor according to Marcus comments

--- src/main/java/io/orten/nano/business/UserAPI.java
+++ src/main/java/io/orten/nano/business/UserAPI.java
@@ -1,8 +1,8 @@
 package io.orten.nano.business;
 
 import io.orten.nano.impl.UserService;
-
 import io.orten.nano.model.User;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,63 +12,63 @@ import java.util.List;
 @Path("/")
 public class UserAPI {
 
-    public static List<User> u_list = new ArrayList<User>();
+    public static List<User> userList = new ArrayList<User>();
 
     @GET
-    @Path("/get/user/{userID}")
+    @Path("/getuser/{userID}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUser(@PathParam("userID") long userID) {
         try {
             User user = UserService.getUser(userID);
-            return Response.status(200).entity(user).build();
+            return Response.status(HttpServletResponse.SC_OK).entity(user).build();
         } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @GET
-    @Path("/get/users")
+    @Path("/getlistofusers")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsers() {
         try {
             List<User> users = UserService.getUsers();
-            return Response.status(200).entity(users).build();
+            return Response.status(HttpServletResponse.SC_OK).entity(users).build();
         } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @GET
-    @Path("/get/users/{userName}")
+    @Path("/getlistofusersbyusername/{userName}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsersByName(@PathParam("userName") String userName) {
         try {
             List<User> users = UserService.getUsersByName(userName);
-            return Response.status(200).entity(users).build();
+            return Response.status(HttpServletResponse.SC_OK).entity(users).build();
         } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @POST
-    @Path("/post/user")
+    @Path("/saveuser")
     public Response saveUser(User user) {
         try {
             UserService.saveUser(user);
-            return Response.status(200).build();
+            return Response.status(HttpServletResponse.SC_OK).build();
         } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @DELETE
-    @Path("/delete/user/{userID}")
-    public Response deleteUser(@PathParam("userID") String userID) {
+    @Path("/deleteuser/{userID}")
+    public Response deleteUser(@PathParam("userID") Long  userID) {
         try {
             UserService.deleteUser(userID);
-            return Response.status(200).build();
+            return Response.status(HttpServletResponse.SC_OK).build();
         } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 }
